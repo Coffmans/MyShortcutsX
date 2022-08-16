cccd05b13006d74126a90300ba3a28e47cfcc998
Coffman <coffmans@gmail.com>
2022-08-16 08:52:30 -0600

    Adding files to project

---
+++ Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShortcutsX
+{
+    public class Constants
+    {
+        public static string StationConfigFile = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + "\\MyShortcutsX\\Shortcuts.XML";
+        public static string CommonAppFolder = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + "\\MyShortcutsX\\";
+    }
+}
